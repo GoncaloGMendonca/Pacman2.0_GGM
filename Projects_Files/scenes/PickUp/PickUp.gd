5c2c1ab97c9fd19b8d659a99ac4bbc0d7a4b8f34
GGM <goncalogm2002@gmail.com>
2023-12-13 21:25:43 +0000

    pick up mecanica feita e correção de bugs

--- Projects_Files/scenes/PickUp/PickUp.gd
+++ Projects_Files/scenes/PickUp/PickUp.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
 const DIRECTIONS := [Vector2.DOWN,Vector2.UP,Vector2.LEFT,Vector2.RIGHT]
+const SPRITES := ["apple","pineapple","bananas","cherry"]
 
-@export var speed := 70 
- 
+var speed := 90
 var direction := Vector2.UP
 var start_position: Vector2
 
+@onready var player_detector: Area2D = %PlayerDetector
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 @onready var stop_move: Timer = %StopMove
 @onready var visibility_timer: Timer = %VisibilityTimer
 
 func _ready() -> void:
+	player_detector.set_collision_mask_value(2,false)
 	GameManager.pacman_died.connect(_restart)
 	start_position = global_position
 	animated_sprite_2d.set_visible(false)
@@ -25,34 +27,23 @@ func _get_next_direction() -> Vector2:
 	return DIRECTIONS.pick_random()
 
 func _restart() -> void:
+	player_detector.set_collision_mask_value(2,false)
 	speed = 70 
 	global_position = start_position
 	direction = DIRECTIONS.pick_random()
 	_get_next_direction()
 
-func _on_player_detector_body_entered(body: Node2D) -> void:
-	print("ETESG")
+func _on_player_detector_body_entered(_body: Node2D) -> void:
 	GameManager.score += 500
 	animated_sprite_2d.set_visible(false)
 	_restart()
-	animated_sprite_2d.play("cherry")
-#	if animated_sprite_2d == "apple":
-#		play("apple")
-#	elif animated_sprite_2d == "cherry":
-#		play("cherry")
-#	elif animated_sprite_2d == "pineapple":
-#		play("pineapple")
-#	elif animated_sprite_2d == "banana":
-#		play("banana")
-#	else:
-#		pass
+	animated_sprite_2d.play(SPRITES.pick_random())
 
 func _on_stop_move_timeout() -> void:
+	player_detector.set_collision_mask_value(2,true)
 	speed = 0
 	animated_sprite_2d.set_visible(true)
 	visibility_timer.start()
-	print("START TIMER")
 
 func _on_visibility_timer_timeout() -> void:
-	print("TEIMEERAD")
 	_restart()
